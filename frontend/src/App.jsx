c6f8033e9822786adc16c0f43d4f535c7562d773
dayong2eyo <dayong2eyo@google.com>
2023-01-17 15:49:20 +0900

    Feature: 리액트 개발환경 세팅

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter as Routes, Route, Router } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
