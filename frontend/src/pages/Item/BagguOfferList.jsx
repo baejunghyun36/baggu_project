0fd062d31faa01ae907bb5178e5ab7ccc8a089fc
dayong2eyo <dayong2eyo@google.com>
2023-01-27 17:51:09 +0900

    Feature:스켈레톤작성

--- frontend/src/pages/Item/BagguOfferList.jsx
+++ frontend/src/pages/Item/BagguOfferList.jsx
@@ -6,11 +6,11 @@ function BagguOfferList({ offers }) {
       <div>
         <span>바꾸신청한 사람들</span>
       </div>
-      {/* {offers.map(offer => (
+      {offers.map(offer => (
         <div key={offer.id}>
           <BagguOfferListItem offer={offer} />
         </div>
-      ))} */}
+      ))}
     </div>
   );
 }
