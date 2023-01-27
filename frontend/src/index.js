f1a03aa2e780ea0bdf539720ab5663ccd4e11393
angielxx <angielxx94@gmail.com>
2023-01-27 09:37:57 +0900

    Feature : twin.macros 기본 세팅 수정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // tailwind
+import GlobalStyles from './styles/GlobalStyles';
 import './styles/tailwind.css';
 import './styles/global.css';
 // import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
