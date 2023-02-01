2e9595871ef32591f95466d0303ba6f0d15d387f
angielxx <angielxx94@gmail.com>
2023-02-01 11:59:14 +0900

    Chore : 누락된 코드 통합

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://baggu.shop:3000';
+
+// 인증이 필요없는 axios 인스턴스
+const axiosApi = (url, options) => {
+  const instance = axios.create({ baseURL: url, ...options });
+  return instance;
+};
+
+// 인증이 필요한 axios 인스턴스
+const axiosAuthApi = (url, options) => {
+  // 토큰 가져오는 코드 수정 필요
+  const token = localStorage.getItem('userInformation');
+  const instance = axios.create({
+    baseURL: url,
+    headers: { Authorization: token },
+    ...options,
+  });
+  return instance;
+};
+
+export const defaultInstance = axiosApi(BASE_URL);
+export const authInstance = axiosAuthApi(BASE_URL);
