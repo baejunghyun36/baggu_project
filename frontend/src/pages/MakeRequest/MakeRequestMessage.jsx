51f9d2b5b89e2f1b742a6ccfc84cf9f71ae80716
angleixx <angielxx94@gmail.com>
2023-02-03 19:47:10 +0900

    Feature : 바꾸신청 페이지 구현 완료

--- frontend/src/pages/MakeRequest/MakeRequestMessage.jsx
+++ frontend/src/pages/MakeRequest/MakeRequestMessage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 // component
@@ -8,6 +8,13 @@ import TopBar2 from 'components/common/TopBar2';
 // twin.macro
 import tw, { styled, css } from 'twin.macro';
 import { useState } from 'react';
+
+// api
+import { post_request } from 'api/apis/request';
+import { post_notify } from 'api/apis/notify';
+
+// store
+import { makeRequestStore } from '../../store/makeRequest';
 
 // Styled Component
 const Wrapper = styled.div``;
@@ -40,21 +47,37 @@ const InputContainer = styled.div`
   }
 `;
 
-// api
-import { get_user_item } from 'api/apis/user';
-// store
-import { makeRequestStore } from '../../store/makeRequest';
-
 function MakeRequestMessage() {
   // API 요청시 파라미터로 전송
   const { itemIdx } = useParams();
 
-  const [message, setMessage] = useState('');
+  const { requestItemIdxList, requestUserIdx, comment, saveComment } =
+    makeRequestStore(state => state);
+  const [message, setMessage] = useState(comment);
 
   const onChangeInput = e => {
-    setMessage(e.currenTarget);
+    setMessage(e.target.value);
+    saveComment(e.target.value);
   };
-  const onClickHandler = () => {};
+  const onClickHandler = () => {
+    const data = {
+      requestItemIdxList: requestItemIdxList,
+      requestUserIdx: requestUserIdx,
+      comment: comment,
+    };
+    console.log(data);
+    // 즉시 실행 익명 함수
+    (async () => {
+      await post_request(itemIdx, data).then(data => {
+        data = {
+          ...data,
+          title: '새로운 바꾸 신청이 도착했습니다.',
+          content: `${data.receiveUserIdx}님과의 물물교환을 시작해보세요.`,
+        };
+        post_notify(data);
+      });
+    })();
+  };
 
   return (
     <div>
@@ -64,8 +87,8 @@ function MakeRequestMessage() {
         <span>생략할 수 있습니다.</span>
       </TextContainer>
       <InputContainer>
-        <input type="text" onChange={onChangeInput} />
-        <p>10자 이하로 작성해주세요.</p>
+        <input type="text" onChange={onChangeInput} value={message} />
+        <p>{message.length ? message.length : '0'}/10</p>
       </InputContainer>
       <FormSubmitBtn
         title="선택 완료"
