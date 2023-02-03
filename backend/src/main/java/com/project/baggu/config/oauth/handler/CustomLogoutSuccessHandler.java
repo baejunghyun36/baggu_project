0a3d0a3103b304aa716673673c48ac3ae375af73
thanku1598 <thanku1598@gmail.com>
2023-02-03 09:30:52 +0900

    Feature:logoutfilter코드작성(에러처리추후필요)

--- backend/src/main/java/com/project/baggu/config/oauth/handler/CustomLogoutSuccessHandler.java
+++ backend/src/main/java/com/project/baggu/config/oauth/handler/CustomLogoutSuccessHandler.java
@@ -1,7 +1,13 @@
 package com.project.baggu.config.oauth.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.baggu.domain.OAuth2KakaoUser;
 import com.project.baggu.domain.RefreshToken;
+import com.project.baggu.dto.AuthLoginDto;
+import com.project.baggu.dto.AuthLogoutDto;
+import com.project.baggu.dto.UserProfileDto;
 import com.project.baggu.service.JwtTokenService;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import javax.servlet.ServletException;
@@ -22,10 +28,29 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
   public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
       Authentication authentication) throws IOException, ServletException {
 
-//    System.out.println(request);
-//    //refresh token 삭제
-//    jwtTokenService.deleteRefreshToken(authentication.getPrincipal().toString());
+    AuthLogoutDto authLogoutDto = getJsonRequest(request);
+    String userIdx = authLogoutDto.getUserIdx().toString();
+
+    //쿠키의 경우 config 설정에 의해 삭제된다.
+    //refresh token을 db에서 삭제한다.
+    //아래 line 500에러 뜸 (findById가 안먹는다..)
+//    jwtTokenService.deleteRefreshToken(userIdx);
 
     System.out.println("로그아웃 완료");
+  }
+  public AuthLogoutDto getJsonRequest(HttpServletRequest request)
+      throws IOException {
+
+    StringBuilder sb = new StringBuilder();
+    BufferedReader reader = request.getReader();
+
+    String line;
+    while ((line = reader.readLine()) != null) {
+      sb.append(line);
+    }
+
+    String jsonPayload = sb.toString();
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.readValue(jsonPayload, AuthLogoutDto.class);
   }
 }
