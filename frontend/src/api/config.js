a35c5942a5688283c33621791c6f414c8ebe93a1
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-02-03 11:47:47 +0900

    Refactor : base_url 설정

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,9 +1,9 @@
 const requests = {
   // baggu 공식
-  // base_url: 'http://i8A708.p.ssafy.io:9999',
+  base_url: 'http://baggu.shop/api',
 
   // 소정님 IP
-  base_url: 'http://70.12.247.158:8080',
+  // base_url: 'http://70.12.247.158:8080',
 
   // 안채님 IP
   // base_url: 'http://70.12.247.174:8080',
