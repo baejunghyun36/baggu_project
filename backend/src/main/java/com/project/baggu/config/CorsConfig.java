b1770ca87b3508c114fe0eac06801c332a6818ea
thanku1598 <thanku1598@gmail.com>
2023-02-07 12:52:12 +0900

    Feature:cors

--- backend/src/main/java/com/project/baggu/config/CorsConfig.java
+++ backend/src/main/java/com/project/baggu/config/CorsConfig.java
@@ -17,8 +17,8 @@ public class CorsConfig {
 
     //origin 설정
     config.addAllowedOrigin("https://test.baggu.shop/**"); //test 서버
-    config.addAllowedOrigin("http://localhost:3000"); //frontend local test용
-    config.addAllowedOrigin("http://localhost:8080"); //backend local test용
+    config.addAllowedOrigin("http://localhost:3000/**"); //frontend local test용
+    config.addAllowedOrigin("http://localhost:8080/**"); //backend local test용
     config.addAllowedOrigin("https://baggu.shop/**"); //deploy 서버
 
     //method 설정
