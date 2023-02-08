1bfca30d55627b44aec01fee708b724e3f388bae
이안채 <dldksco@naver.com>
2023-02-08 11:08:41 +0900

    Feature:logout구현

--- backend/src/main/java/com/project/baggu/controller/UserController.java
+++ backend/src/main/java/com/project/baggu/controller/UserController.java
@@ -16,6 +16,8 @@ import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
